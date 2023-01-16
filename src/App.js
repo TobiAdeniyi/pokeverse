6b9d42a70d9581271b6099a89ba125ec84680360
Tobi Adeniyi <tobiadeniyi@fb.com>
2023-01-16 15:21:31 +0000

    Update: initial update for pokeverse

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { InputGroup, Form, Container } from 'react-bootstrap';
 import { Navigation } from './components/Navigation';
 import { PokemonCard } from './components/PokemonCard';
 
@@ -6,13 +7,42 @@ const LIMIT = 150;
 const pokeApi = `https://pokeapi.co/api/v2/pokemon/?limit=${LIMIT}`;
 
 function App() {
+  const [pokemonList, setPokemonList] = useState([]);
+  const [filteredPokemonList, setFilteredPokemonList] = useState([]);
+
+  useEffect(() => {
+    fetch(pokeApi)
+      .then(res => res.json())
+      .then(data => {
+        setPokemonList(data.results);
+        setFilteredPokemonList(data.results);
+      })
+      .catch(error => console.error(error));
+  }, []);
 
   return (
     <div data-testid="app">
       <Navigation />
-
+      <InputGroup className="mb-3">
+        <InputGroup.Text id="pokemon-search">Search</InputGroup.Text>
+        <Form.Control
+          placeholder="Pokemon"
+          aria-label="Pokemon"
+          aria-describedby="pokemon-search"
+          onChange={e => setFilteredPokemonList(pokemonList.filter(
+            pokemon => pokemon.name.includes(e.target.value.trim())
+          ))} />
+      </InputGroup>
       <h1>Pokemon should appear here</h1>
-      <PokemonCard />
+      <Container>
+        <div>{
+          filteredPokemonList.map(pokemonData => (
+            <PokemonCard
+              url={pokemonData.url}
+              name={pokemonData.name} />
+          ))
+        }</div>
+      </Container>
     </div>
   );
 }
--- src/components/PokemonCard.js
+++ src/components/PokemonCard.js
@@ -1,11 +1,44 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Card, ListGroup } from 'react-bootstrap';
 
 function PokemonCard({ url, name }) {
+  const [pokemonData, setPokemonData] = useState(null);
+
+  useEffect(() => {
+    fetch(url)
+      .then(res => res.json())
+      .then(data => setPokemonData(data))
+      .catch(error => console.error(error));
+  }, [url]);
 
   return (
     <div>
-        pokemon card
-    </div>
+      <Card style={{ width: '18rem' }} className="mx-auto sm">
+        <Card.Img
+          bg="dark"
+          width="286"
+          height="286"
+          variant="top"
+          text="white"
+          src={pokemonData?.sprites.front_default} />
+        <Card.Body >
+          <Card.Title className="h1">{name.toUpperCase()}</Card.Title>
+          <Card.Subtitle className="mb-2 text-muted">{pokemonData?.species.name}</Card.Subtitle>
+          <Card.Text as="div">
+            <h2>Abilities:</h2>
+            <ul>
+              <ListGroup variant="flush">
+                {pokemonData?.abilities.map(ability => (
+                  <ListGroup.Item key={ability.ability.name}>
+                    {ability.ability.name}
+                  </ListGroup.Item>
+                ))}
+              </ListGroup>
+            </ul>
+          </Card.Text>
+        </Card.Body>
+      </Card>
+    </div >
   );
 }
 
